b2d8bab3c04524bb1d95c8182be71903c1c2710c
Talha <tasezgi.n@gmail.com>
2019-11-26 22:33:38 +1100

    Tracking method to estimate endpoints between JSON calls

--- src/main.cpp
+++ src/main.cpp
@@ -33,12 +33,23 @@ double servoPosition[2];
 double servoStart[] = {servoBotMin,servoTopMin};
 double servoMid[] = {((servoBotMax-servoBotMin)/2) + servoBotMin,((servoTopMax-servoTopMin)/2) + servoTopMin};
 double servoEnd[] = {servoBotMax,servoTopMax};
+double acTimePos, acLat, acLon, acAlt, acOnGround, acVelocity, acTTrack, acVRate;
+//
+double bearingToAc;
 
 
 
 // Functions
+float toRadians(float angle) {
+  return (PI / 180) * angle;
+}
 
-void positionServos(String position = "NA") {
+float toDegrees(float rad) {
+  return (rad * 180) / PI;
+}
+
+//Sets and moves the position of the servos in servoPosition[] according to start,mid,end
+void setServosDef(String position = "NA") {
   if(position == "start") {
     Serial.println("Servo pos  = start ");
     servoPosition[0] = servoStart[0];
@@ -67,7 +78,8 @@ void positionServos(String position = "NA") {
 
   }
 
-void positionServosXY(int servoBotPosition = servoPosition[0], int ServoTopPosition = servoPosition[1] ){
+//Moves servos to currently stored positions in servoBotPosition and ServoTopPosition unless passed into function as microseconds
+void setServosXY(int servoBotPosition = servoPosition[0], int ServoTopPosition = servoPosition[1] ){
   Serial.print("servo bot moved to:");
   Serial.println(servoBotPosition);
   Serial.print("servo top moved to:");
@@ -95,61 +107,57 @@ void bootSequence() {
 
 }
 
-  float toRadians(float angle) {
-  return (PI / 180) * angle;
-}
-
-  float toDegrees(float rad) {
-    return (rad * 180) / PI;
-}
  
-  int getDirection(float latitude1, float longitude1, float latitude2, float longitude2) {
-    float lat1 = toRadians(latitude1);
-    float lat2 = toRadians(latitude2);
-    float lng1 = toRadians(longitude1);
-    float lng2 = toRadians(longitude2);
-    float Y = sin(lng2 - lng1) * cos(lat2);
-    float X = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(lng2 - lng1);  
-    float deg = toDegrees(atan2(Y, X));  
-    // note that this implementation doesn't use the module, but angles lower than 0 get augmented by 360 only
-    if (deg < 0) {
-      deg = 360 + deg;
-    }
-    float angle = deg;
-    int a = (int) (abs(angle) + (1 / 7200));
-    return a;
+// Calculate bearing from servo to AC
+int bearingServoToAC(float latitude1, float longitude1, float latitude2, float longitude2) {
+  float lat1 = toRadians(latitude1);
+  float lat2 = toRadians(latitude2);
+  float lng1 = toRadians(longitude1);
+  float lng2 = toRadians(longitude2);
+  float Y = sin(lng2 - lng1) * cos(lat2);
+  float X = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(lng2 - lng1);  
+  float deg = toDegrees(atan2(Y, X));  
+  // note that this implementation doesn't use the module, but angles lower than 0 get augmented by 360 only
+  if (deg < 0) {
+    deg = 360 + deg;
+  }
+  float angle = deg;
+  int a = (int) (abs(angle) + (1 / 7200));
+  return a;
 }
 
-  double bearingtoX(double bearing){
-    if(bearing>=270 && bearing < 359) {
-      servoPosition[0]= 0;
-      servoPosition[1]= servoStart[1] + (((int)bearing % 270) / q1Multiplier);
-    } else if(bearing>=0 && bearing < 89) {
-      servoPosition[0]= 0;
-      servoPosition[1]= servoMid[1] + (bearing*q2Multiplier);
-    } else if(bearing>=90 && bearing < 179) {
-      servoPosition[0]= 180;
-      servoPosition[1]= servoMid[1] + (bearing*q2Multiplier);
-    } else if(bearing>=180 && bearing < 269) {
-      servoPosition[0]= 180;
-      servoPosition[1]= servoStart[1] + (((int)bearing % 180) / q4Multiplier);;
+//Convert bearing from ADSB data to X co-ordinate for top servo
+void bearingtoX(double bearing){
+  if(bearing>=270 && bearing < 359) {
+    servoPosition[0]= 0;
+    servoPosition[1]= servoStart[1] + (((int)bearing % 270) / q1Multiplier);
+  } else if(bearing>=0 && bearing < 89) {
+    servoPosition[0]= 0;
+    servoPosition[1]= servoMid[1] + (bearing*q2Multiplier);
+  } else if(bearing>=90 && bearing < 179) {
+    servoPosition[0]= 180;
+    servoPosition[1]= servoMid[1] + (bearing*q2Multiplier);
+  } else if(bearing>=180 && bearing < 269) {
+    servoPosition[0]= 180;
+    servoPosition[1]= servoStart[1] + (((int)bearing % 180) / q4Multiplier);;
 
-    } else Serial.println("Bearing Out Of Bounds");
-    Serial.print("calculated servoPosition[1] ======> ");
-    Serial.println(servoPosition[1]);
-    delay(15);
-    if(servoPosition[1] >= servoTopMax) {
-      servoPosition[1] = servoTopMax;
-      Serial.println("Hit max");
-      }
-    if(servoPosition[1] <= servoTopMin) {
-      servoPosition[1] = servoTopMin;
-      Serial.println("Hit min");
+  } else Serial.println("Bearing Out Of Bounds");
+  Serial.print("calculated servoPosition[1] ======> ");
+  Serial.println(servoPosition[1]);
+  delay(15);
+  if(servoPosition[1] >= servoTopMax) {
+    servoPosition[1] = servoTopMax;
+    Serial.println("Hit max");
     }
-
+  if(servoPosition[1] <= servoTopMin) {
+    servoPosition[1] = servoTopMin;
+    Serial.println("Hit min");
   }
 
-double calculateDistance(double currentLat,double currentLon,double acLat,double acLon)
+}
+
+//Returns the distance between the servos and the Aircraft
+double calcServoToAc(double currentLat,double currentLon,double acLat,double acLon)
 {
 	double dx, dy, dz;
 	currentLon -= acLon;
@@ -161,7 +169,26 @@ double calculateDistance(double currentLat,double currentLon,double acLat,double
 	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * eaRa;
 }
 
+//returns km for m/s and seconds input
+double calcDistance(double speedMsec, double timeSec ) {
+  return speedMsec / timeSec; 
+}
 
+
+//Estimate end point in 10 secs between JSON calls so vector can be made
+void trackAC(double acLat, double acLon, int bearing, double distanceKM) {
+  acLat *= toRad, acLon *= toRad;
+
+  double lat2 = asin( sin(acLat)*cos(distanceKM/eaRa) + cos(acLat)*sin(distanceKM/eaRa)*cos(bearing));
+  double lon2 = acLon + atan2(sin(bearing)*sin(distanceKM/eaRa)*cos(acLat),cos(distanceKM/eaRa)-sin(acLat)*sin(lat2));
+
+Serial.print("lat AC dest ======> ");
+Serial.println(toDegrees(lat2));
+Serial.print("lon AC dest ======> ");
+Serial.println(toDegrees(lon2));
+setServosXY
+
+}
 
 
 
@@ -184,7 +211,7 @@ void setup()
 
 void loop()
 {
-    const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(17) + JSON_OBJECT_SIZE(2) + 50;
+  const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_ARRAY_SIZE(17) + JSON_OBJECT_SIZE(2) + 50;
   DynamicJsonDocument doc(capacity);
 
   const char* json = "{\"time\":1574661430,\"states\":[[\"7c6ae1\",\"VEB     \",\"Australia\",1574661430,1574661430,145.1089,-37.5592,2339.34,false,93.35,30.1,6.83,null,2331.72,\"3673\",false,0]]}";
@@ -194,77 +221,65 @@ void loop()
   long time = doc["time"]; // 1574661430
 
   JsonArray states_0 = doc["states"][0];
-  const char* states_0_0 = states_0[0]; // "7c6ae1"
-  const char* states_0_1 = states_0[1]; // "VEB     "
-  const char* states_0_2 = states_0[2]; // "Australia"
-  long states_0_3 = states_0[3]; // 1574661430
-  long states_0_4 = states_0[4]; // 1574661430
-  double states_0_5 = states_0[5]; // 145.1089
-  double states_0_6 = states_0[6]; // -37.5592
-  double states_0_7 = states_0[7]; // 2339.34
-  bool states_0_8 = states_0[8]; // false
-  double states_0_9 = states_0[9]; // 93.35
-  double states_0_10 = states_0[10]; // 30.1
-  double states_0_11 = states_0[11]; // 6.83
-  double states_0_13 = states_0[13]; // 2331.72
-  const char* states_0_14 = states_0[14]; // "3673"
-  bool states_0_15 = states_0[15]; // false
-  int states_0_16 = states_0[16]; // 0
-   Serial.println("finish setup");
+  acTimePos = states_0[3]; // 1574661430
+  acLon = states_0[5]; // 145.1089
+  acLat = states_0[6]; // -37.5592
+  acAlt = states_0[7]; // 2339.34
+  acOnGround = states_0[8]; // false
+  acVelocity = states_0[9]; // 93.35
+  acTTrack = states_0[10]; // 30.1
+  acVRate = states_0[11]; // 6.83
 
-  double distance  = calculateDistance(currentLat,currentLon,states_0_6,states_0_5);
-  Serial.print("distance  out of loop:");
-  Serial.println(distance);
-  delay(1000);
+for (size_t i = 0; i < 10; i++){
+  bearingToAc = bearingServoToAC(currentLat,currentLon,acLat,acLon);
 
+  Serial.print("bearingToAc :");
+  Serial.println(bearingToAc);
 
-for (size_t i = 0; i < 10; i++)
-{
-  
+  bearingtoX(bearingToAc);
 
-
-double direction = getDirection(currentLat,currentLon,states_0_6,states_0_5);
-Serial.print("direction :");
-Serial.println(direction);
-bearingtoX(direction);
- Serial.print("servobotpos and top pos");
+  Serial.print("moving servos to bearing from servo");
   Serial.println(servoPosition[0]);
   Serial.println(servoPosition[1]);
   Serial.println("");
-positionServosXY();
-delay(1000);
-  states_0_5 -= .01;
-  /* code */
 
+  setServosXY();
+
+  delay(1000);
+
+  trackAC(acLat,acLon, acTTrack, .9  );
+
+  //acLon -= .01;
+  
   Serial.println("");
   Serial.println("");
-  double distance  = calculateDistance(currentLat,currentLon,states_0_6,states_0_5);
+  double distance  = calcServoToAc(currentLat,currentLon,acLat,acLon);
   Serial.print("distance in loop :");
   Serial.println(distance);
   delay(1000);
   Serial.println("");
   Serial.println("");
 
-}
+  }
 
 
 
-/* Serial.println("start");
-positionServos("start");
-delay(1000);
-Serial.println("mid");
-positionServos("mid");
-delay(1000);
-Serial.println("end");
-positionServos("end");
-delay(1000);
-bootSequence();
-servoBot.writeMicroseconds(500);              // tell servo to go to position in variable 'pos'
-delay(2000);                       // waits 15ms for the servo to reach the position
-servoTop.writeMicroseconds(500);              // tell servo to go to position in variable 'pos'
-delay(500);                       // waits 15ms for the servo to reach the position
+  /* Serial.println("start");
+  positionServos("start");
+  delay(1000);
+  Serial.println("mid");
+  positionServos("mid");
+  delay(1000);
+  Serial.println("end");
+  positionServos("end");
+  delay(1000);
+  bootSequence();
+  servoBot.writeMicroseconds(500);              // tell servo to go to position in variable 'pos'
+  delay(2000);                       // waits 15ms for the servo to reach the position
+  servoTop.writeMicroseconds(500);              // tell servo to go to position in variable 'pos'
+  delay(500);                       // waits 15ms for the servo to reach the position
 
- */
+  */
 }
 
 
